3de2297c0827fa56af1b51bbe460c55df0b68966
slowdolphin <tighe59@gmail.com>
2021-05-14 21:18:17 -0400

    created our enemy pizza rat

--- donut_adventure/adventure.js
+++ donut_adventure/adventure.js
@@ -20,3 +20,25 @@ class Hero {
 }
 
 const dougie = new Hero('Dougie the Donut')
+
+class Enemy {
+  constructor(name){
+    this.name = name;
+    this.health = 100;
+    this.weapons = {
+      pepperoniStars: 5, cheeseGrease: 10
+    }
+    this.catchPhrase = ["I'm youtube Famous", "I'm more dangerous than an uncovered sewer"]
+  }
+  talkSass(){
+    console.log(this.catchPhrase[Math.floor(Math.random() * this.catchPhrase.length))
+  }
+  announceHealth(){
+    console.log(this.health)
+  }
+  fight(){
+    console.log("I'm gonna flatten you like a slice of pepperoni!")
+  }
+}
+
+const ratBoi = new Enemy ('Pizza Rat')
